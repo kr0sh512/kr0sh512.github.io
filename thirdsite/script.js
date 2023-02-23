c19f24a18641a90dcc4f7980a113a2de2e7f760d
kr0sh512 <dimaam2015@yandex.ru>
2023-02-24 01:45:00 +0300

    1

--- thirdsite/script.js
+++ thirdsite/script.js
@@ -1,27 +1,3 @@
-function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-        currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
-}
-
-// function animateShow(id) {
-//     delay = 60;
-//     $.fn.animate_Text = function () {
-//         var string = this.text();
-//         return this.each(function () {
-//             var $this = $(this);
-//             $this.html(string.replace(/./g, '<span class="new">$&</span>'));
-//             $this.find('span.new').each(function (i, el) {
-//                 setTimeout(function () { $(el).addClass('div_opacity'); }, delay * i);
-//             });
-//         });
-//     };
-//     $('#b' + parseInt(id) + ' #rg_text').show();
-//     $('#b' + parseInt(id) + ' #rg_text').animate_Text();
-// }
-
 function animateShow(id) {
     delay = 60;
     elem = $('#b' + parseInt(id) + ' .rg_text');
@@ -38,38 +14,9 @@ function animateShow(id) {
     return (string.length * delay);
 }
 
-// function func2() {
-//     t = 0;
-//     fadetime = 600;
-//     between = 1400;
-//     for (let i = 1; i < 4; ++i) {
-//         setTimeout(function () {
-//             animateShow(i);
-//         }, t);
-//         t += $('#b' + parseInt(i) + ' #rg_text').text().length * 60;
-
-//         setTimeout(function () {
-//             document.querySelector('#b' + parseInt(i) + ' #rg_text').style.display = "none";
-//             document.querySelector('#b' + parseInt(i) + ' #st_text').style.display = "block";
-//             $('#b' + parseInt(i) + ' #st_text').addClass('st');
-//         }, t);
-
-//         t += between;
-
-//         if (i == 3) break;
-//         setTimeout(function () {
-//             $('#b' + parseInt(i) + ' #st_text').fadeOut(fadetime);
-//         }, t);
-//         setTimeout(function () {
-//             document.getElementById('b' + parseInt(i)).style.display = "none";
-//         }, t + fadetime);
-//         t += fadetime;
-//     }
-// }
-
 isRun = false;
 
-function func2(i) {
+function startAnim(i) {
     animateShow(i);
     t = $('#b' + parseInt(i) + ' .rg_text').text().length * 60 + 100;
     setTimeout(function () {
@@ -95,18 +42,16 @@ document.onclick = function () {
         isRun = true;
         fade(it - 1);
         setTimeout(function () {
-            func2(it);
+            startAnim(it);
             it++;
         }, 600 + 200);
     }
-    console.log(123);
 };
 
 document.addEventListener("DOMContentLoaded", function () {
     setTimeout(function () {
         document.getElementById("load_outer").style.display = "none";
-        func2(1);
+        startAnim(1);
     }, 400);
-    //$('#b1 #rg_text').fadeOut(300);
 });
 
