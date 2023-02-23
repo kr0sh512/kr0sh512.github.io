854c7aa43062e589fe2e3ce83bd0097943fb5d7b
kr0sh512 <dimaam2015@yandex.ru>
2023-02-24 01:38:55 +0300

    a littl for corp

--- CorpWebSite/contacts/send_email.php
+++ CorpWebSite/contacts/send_email.php
@@ -4,14 +4,14 @@
     $email = $_POST['email'];
     $question = $_POST['question'];
     $recipient1 = 'sergey.maslov58@yandex.ru';
-    $recipient2 = 'vika_gudova87@mail.ru';
-
+    
     $subject = 'Заявка на сайте maslov58.ru';
     $message = "Имя: $name\nТелефон: $phone\nEmail: $email\nСообщение:\n$question";
     $headers = 'From: webmaster@maslov58.ru' . "\r\n" .
-               'Reply-To: webmaster@maslov58.ru' . "\r\n" .
-               'X-Mailer: PHP/' . phpversion();
-
+    'Reply-To: webmaster@maslov58.ru' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    
     mail($recipient1, $subject, $message, $headers);
-    mail($recipient2, $subject, $message, $headers);
+    // $recipient2 = 'vika_gudova87@mail.ru';
+    // mail($recipient2, $subject, $message, $headers);
 ?>
--- CorpWebSite/resources/scripts/script.js
+++ CorpWebSite/resources/scripts/script.js
@@ -1,30 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => { // DOM готов к взаимодейтсвию
-
-    const onScrollHeader = () => { // объявляем основную функцию onScrollHeader
-
-        const nav = document.querySelector('nav') // находим header и записываем в константу
-
-        let prevScroll = window.pageYOffset // узнаем на сколько была прокручена страница ранее
-        let currentScroll // на сколько прокручена страница сейчас (пока нет значения)
-
-        window.addEventListener('scroll', () => { // при прокрутке страницы
-
-            currentScroll = window.pageYOffset // узнаем на сколько прокрутили страницу
-
-            const headerHidden = () => nav.classList.contains('header_hidden') // узнаем скрыт header или нет
-
-            if (currentScroll > prevScroll && !headerHidden()) { // если прокручиваем страницу вниз и header не скрыт
-                nav.classList.add('header_hidden') // то скрываем header
-            }
-            if (currentScroll < prevScroll && headerHidden()) { // если прокручиваем страницу вверх и header скрыт
-                nav.classList.remove('header_hidden') // то отображаем header
-            }
-            prevScroll = currentScroll // записываем на сколько прокручена страница на данный момент
-
-        })
-
-    }
-
-    onScrollHeader() // вызываем основную функцию onScrollHeader
-
+document.addEventListener("DOMContentLoaded", function () {
+    $("a").click(function () {
+        var href = $(this).attr("href");
+        setTimeout(function () {
+            window.location = href;
+        }, 200);
+        return false;
+    });
 });
